53d93417cbc15f980bffd92bd338539353549d34
Vinit <vinitsk99@gmail.com>
2024-07-13 17:21:20 +0530

    Add MongoDB connection and setup server

--- back-end/index.js
+++ back-end/index.js
@@ -3,8 +3,27 @@ import bodyParser from "body-parser"
 import dotenv from "dotenv"
 import cors from "cors"
 import mongoose from "mongoose"
+import route from "./routes/user.route.js"
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config() //PORT and URL 
+
+
+const PORT = process.env.PORT || 7000;
+const URL = process.env.URL;
+
+mongoose.connect(URL).then(()=>{
+    console.log("DB CONNECTED!!");
+
+    app.listen(PORT, ()=>{
+        console.log(`server is running on PORT: ${PORT}`);
+    })
+
+}).catch(error => {
+    console.log(error)
+}
+)
+
+app.use("/api", route)
