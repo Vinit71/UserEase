f2ceb31f3119b9028aa7480daf38243acf9d8ce3
Vinit <vinitsk99@gmail.com>
2024-07-14 22:54:53 +0530

    Add routes

--- back-end/routes/user.route.js
+++ back-end/routes/user.route.js
@@ -1,9 +1,12 @@
 import {Router} from "express"
-import { create } from "../controller/userControler.js";
+import { create, fetchAll, fetchOne, removeUser, updateDetails } from "../controller/userControler.js";
 
 const route = Router();
 
 //route for create user
 route.post("/create", create)
-
+route.get("/fetchAll", fetchAll)
+route.get("/fetchOne/:id", fetchOne)    //based upon user's id
+route.put("/updateDetails/:id", updateDetails)  //based upon user's id
+route.delete("/deleteUser/:id", removeUser) //based upon user's id
 export default route;
