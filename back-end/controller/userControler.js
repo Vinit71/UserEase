b3ba38b71227e660756c9f62d2e78c20ddd12425
Vinit <vinitsk99@gmail.com>
2024-07-14 22:53:30 +0530

    Implement CRUD API endpoints

--- back-end/controller/userControler.js
+++ back-end/controller/userControler.js
@@ -16,4 +16,64 @@ export const create = async(req, res)=> {
     }
 }
 
+//fetch data (read)
+
+export const fetchAll = async(req, res) =>{
+    try {
+        const userData = await User.find();
+        if(!userData){
+            return res.status(404).json("User not found")
+        }
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+            //fetch One user by id
+export const fetchOne = async(req, res) => {
+    try {
+        const userId = req.params.id;
+        const userData = await User.findById(userId);
+        if(!userData){
+            return res.status(401).json("User not exist");
+        }
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+//Update - based upon user's id
+export const updateDetails = async(req, res) => {
+    try {
+        const userId = req.params.id;
+        const userData = await User.findById(userId);
+        if(!userData){
+            return res.status(401).json("User not exist")
+        }
+
+        //updation part
+        const updateData = await User.findByIdAndUpdate(userId, req.body, {new: true})
+        res.status(200).json(updateData);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+//Delete
+export const removeUser = async(req, res) => {
+    try {
+        const userId = req.params.id;
+        const userExists = await User.findById(userId);
+        if(!userExists){
+            return res.status(404).json("User do not exists")
+        }
+
+        //Deletion part
+        await User.findByIdAndDelete(userId)
+        res.status(200).json("User removed successfully");
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
 
